2620f5508b6ea27fe5d969550ba6ba0acdea08f6
Bob Corsaro <bob@embed.ly>
2011-12-02 14:48:52 -0500

    Demo

--- htdocs/js/jquery.embedly-socket.js
+++ htdocs/js/jquery.embedly-socket.js
@@ -1,29 +1,39 @@
 var socket = io.connect("/");
+
+function resize(img) {
+  return 'http://i.embed.ly/?key=dededededededededededededededede&url='+escape(img)+'&width=120&height=80&crop=true';
+}
+
 socket.on('response', function(data) {
   console.log(data)
   var preview = data[2][0];
   if (preview.type == 'error') {
-    $('#feed').append('<div><img src="/images/error.png"/></div>')
+    $('#'+data[1]._id).replaceWith('<div><img class="thumbnail" src="images/error.png"/></div>')
   } else {
-    $('#feed').append('<div><img src="'+data[2][0].images[0].url+'"/></div>')
+    $('#'+data[1]._id).replaceWith('<div><div style="clear: both;"></div><h4 class="title"><a href="'+preview.url+'">'+preview.title+'</a></h4><div><a href="'+preview.url+'"><img class="thumbnail" src="'+resize(data[2][0].images[0].url)+'"/></a><p class="description">'+preview.description+'</p></div></div>')
   }
 });
 socket.on('error', function(data) {
   console.error('A error occurred!')
   console.error(data)
-  $('#feed').append('<div><img src="/images/error.png"/></div>')
+  $('#'+data[1]._id).replaceWith('<div><img class="thumbnail" src="images/error.png"/></div>')
 });
+
 socket.on('timeout', function(data) {
   console.error('A timeout occurred!')
   console.error(data)
-  $('#feed').append('<div><img src="/images/error.png"/></div>')
+  $('#'+data[1]._id).replaceWith('<div><img class="thumbnail" src="images/error.png"/></div>')
 });
+
 var FORCE=false;
+var id = 0;
 jQuery(document).ready(function onready($) {
   console.log('ready');
-  $('#id_submit').click(function submit_click(ev) {
+  $('#id_attach').click(function submit_click(ev) {
+    id += 1;
     ev.preventDefault();
-    socket.emit('preview', {url: $('#id_status').val(), force: FORCE});
+    $('#feed').prepend('<div id="'+id+'"><img class="thumbnail" src="images/loading-rectangle.gif"/></div>')
+    socket.emit('preview', {url: $('#id_url').val(), words: 20, _id: id, force: FORCE});
   });
 })
 
